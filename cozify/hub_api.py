6fe0d11f4f917416050fdcb319505ae064744110
Ville Savolainen <villes@villes.fi>
2025-06-06 19:20:38 +0300

    Add support for query parameters in API calls and implement new endpoints for groups, rules, devices, and hub configuration

--- cozify/hub_api.py
+++ cozify/hub_api.py
@@ -20,13 +20,14 @@ def _getBase(host, port=8893, **kwargs):
     return "http://{0}:{1}".format(host, port)
 
 
-def get(call, hub_token_header=True, base=apiPath, **kwargs):
+def get(call, hub_token_header=True, base=apiPath, params=None, **kwargs):
     """GET method for calling hub API.
 
     Args:
         call(str): API path to call after apiPath, needs to include leading /.
         hub_token_header(bool): Set to False to omit hub_token usage in call headers.
         base(str): Base path to call from API instead of global apiPath. Defaults to apiPath.
+        params(dict): Query parameters to include in the request.
         **host(str): ip address or hostname of hub.
         **hub_token(str): Hub authentication token.
         **remote(bool): If call is to be local or remote (bounced via cloud).
@@ -36,11 +37,12 @@ def get(call, hub_token_header=True, base=apiPath, **kwargs):
         method=requests.get,
         call="{0}{1}".format(base, call),
         hub_token_header=hub_token_header,
+        params=params,
         **kwargs
     )
 
 
-def put(call, data, hub_token_header=True, base=apiPath, **kwargs):
+def put(call, data, hub_token_header=True, base=apiPath, params=None, **kwargs):
     """PUT method for calling hub API. For rest of kwargs parameters see get()
 
     Args:
@@ -48,22 +50,25 @@ def put(call, data, hub_token_header=True, base=apiPath, **kwargs):
         data(str): json string to push out as the payload.
         hub_token_header(bool): Set to False to omit hub_token usage in call headers.
         base(str): Base path to call from API instead of global apiPath. Defaults to apiPath.
+        params(dict): Query parameters to include in the request.
     """
     return _call(
         method=requests.put,
         call="{0}{1}".format(base, call),
         hub_token_header=hub_token_header,
         data=data,
+        params=params,
         **kwargs
     )
 
 
-def _call(*, call, method, hub_token_header, data=None, **kwargs):
+def _call(*, call, method, hub_token_header, data=None, params=None, **kwargs):
     """Backend for get & put
 
     Args:
         call(str): Full API path to call.
         method(function): requests.get|put function to use for call.
+        params(dict): Query parameters to include in the request.
     """
     response = None
     headers = {}
@@ -84,7 +89,7 @@ def _call(*, call, method, hub_token_header, data=None, **kwargs):
             raise AttributeError("Asked to do remote call but no cloud_token provided.")
         headers["Authorization"] = kwargs["cloud_token"]
         headers["X-Hub-Key"] = kwargs["hub_token"]
-        response = cloud_api.remote(apicall=call, data=data, headers=headers)
+        response = cloud_api.remote(apicall=call, data=data, headers=headers, params=params)
     else:  # local call
         if "host" not in kwargs or not kwargs["host"]:
             raise AttributeError(
@@ -92,7 +97,7 @@ def _call(*, call, method, hub_token_header, data=None, **kwargs):
             )
         try:
             response = method(
-                _getBase(**kwargs) + call, headers=headers, data=data, timeout=5
+                _getBase(**kwargs) + call, headers=headers, data=data, params=params, timeout=5
             )
         except requests.exceptions.RequestException as e:  # pragma: no cover
             raise ConnectionError(str(e)) from None
@@ -262,3 +267,196 @@ def scenes_command_on(scene_id, **kwargs):
     return scenes_command_state(
         scene_id=scene_id, request_type="CMD_SCENE_ON", **kwargs
     )
+
+
+def groups(**kwargs):
+    """Implementation of /groups API call. For kwargs see cozify.hub_api.get()
+
+    Returns:
+        dict: Full groups state as returned by the API
+    """
+    return get("/groups", **kwargs)
+
+
+def groups_command(command, **kwargs):
+    """1:1 implementation of /groups/command. For kwargs see cozify.hub_api.put()
+
+    Args:
+        command(dict): dictionary containing the changes wanted. Will be converted to json.
+
+    Returns:
+        str: What ever the API replied or raises an APIError on failure.
+    """
+    logging.debug("command json to send: {0}".format(command))
+    return put("/groups/command", command, **kwargs)
+
+
+def groups_command_state(*, group_id, request_type, **kwargs):
+    """Commands changing a group's state. For kwargs see cozify.hub_api.put()
+
+    Args:
+        group_id(str): ID of the group to operate on.
+        request_type(str): the request type, i.e. CMD_GROUP_ON or CMD_GROUP_OFF.
+    Returns:
+        str: Whatever the API replied or raises an APIError on failure.
+    """
+    command = [{"id": group_id, "type": request_type}]
+    logging.debug("command json to send: {0}".format(command))
+    return put("/groups/command", command, **kwargs)
+
+
+def groups_command_off(group_id, **kwargs):
+    """Command helper for CMD_GROUP_OFF.
+
+    Args:
+        group_id(str): ID of the group to operate on.
+    Returns:
+        str: What ever the API replied or raises an APIException on failure.
+    """
+    return groups_command_state(
+        group_id=group_id, request_type="CMD_GROUP_OFF", **kwargs
+    )
+
+
+def groups_command_on(group_id, **kwargs):
+    """Command helper for CMD_GROUP_ON.
+
+    Args:
+        group_id(str): ID of the group to operate on.
+    Returns:
+        str: What ever the API replied or raises an APIException on failure.
+    """
+    return groups_command_state(
+        group_id=group_id, request_type="CMD_GROUP_ON", **kwargs
+    )
+
+
+def rules(**kwargs):
+    """Implementation of /rules API call. For kwargs see cozify.hub_api.get()
+
+    Returns:
+        dict: Full rules state as returned by the API
+    """
+    return get("/rules", **kwargs)
+
+
+def rules_command(command, **kwargs):
+    """1:1 implementation of /rules/command. For kwargs see cozify.hub_api.put()
+
+    Args:
+        command(dict): dictionary containing the changes wanted. Will be converted to json.
+
+    Returns:
+        str: What ever the API replied or raises an APIError on failure.
+    """
+    logging.debug("command json to send: {0}".format(command))
+    return put("/rules/command", command, **kwargs)
+
+
+def rules_command_state(*, rule_id, request_type, **kwargs):
+    """Commands changing a rule's state. For kwargs see cozify.hub_api.put()
+
+    Args:
+        rule_id(str): ID of the rule to operate on.
+        request_type(str): the request type, i.e. CMD_RULE_ON or CMD_RULE_OFF.
+    Returns:
+        str: Whatever the API replied or raises an APIError on failure.
+    """
+    command = [{"id": rule_id, "type": request_type}]
+    logging.debug("command json to send: {0}".format(command))
+    return put("/rules/command", command, **kwargs)
+
+
+def rules_command_off(rule_id, **kwargs):
+    """Command helper for CMD_RULE_OFF.
+
+    Args:
+        rule_id(str): ID of the rule to operate on.
+    Returns:
+        str: What ever the API replied or raises an APIException on failure.
+    """
+    return rules_command_state(
+        rule_id=rule_id, request_type="CMD_RULE_OFF", **kwargs
+    )
+
+
+def rules_command_on(rule_id, **kwargs):
+    """Command helper for CMD_RULE_ON.
+
+    Args:
+        rule_id(str): ID of the rule to operate on.
+    Returns:
+        str: What ever the API replied or raises an APIException on failure.
+    """
+    return rules_command_state(
+        rule_id=rule_id, request_type="CMD_RULE_ON", **kwargs
+    )
+
+
+def devices_configuration(device_id=None, **kwargs):
+    """Implementation of /devices/configuration API call. For kwargs see cozify.hub_api.get()
+
+    Args:
+        device_id(str): Optional ID of the device to get configuration for.
+    Returns:
+        dict: Device configuration as returned by the API
+    """
+    params = {}
+    if device_id:
+        params = {"deviceId": device_id}
+    return get("/devices/configuration", params=params, **kwargs)
+
+
+def devices_configuration_save(configuration, **kwargs):
+    """Implementation of /devices/configuration API call. For kwargs see cozify.hub_api.put()
+
+    Args:
+        configuration(dict): Device configuration to save.
+    Returns:
+        str: What ever the API replied or raises an APIError on failure.
+    """
+    return put("/devices/configuration", configuration, **kwargs)
+
+
+def hub_autoconfig(configuration, **kwargs):
+    """Implementation of /hub/autoconfig API call. For kwargs see cozify.hub_api.put()
+
+    Args:
+        configuration(dict): Configuration data to apply.
+    Returns:
+        bool: True on success, otherwise error
+    """
+    return put("/hub/autoconfig", configuration, **kwargs)
+
+
+def hub_poll(ts=None, device_ts=None, group_ts=None, scene_ts=None, rule_ts=None, activator_ts=None, cozify_uuid=None, **kwargs):
+    """Implementation of /hub/poll API call. For kwargs see cozify.hub_api.get()
+
+    Args:
+        ts(int): Timestamp for all data. If specified result contains all hub data that has changed after the timestamp.
+        device_ts(int): Timestamp for device data. If specified result contains device data that has changed after this timestamp.
+        group_ts(int): Timestamp for group data. If specified result contains group data that has changed after this timestamp.
+        scene_ts(int): Timestamp for scene data. If specified result contains scene data that has changed after this timestamp.
+        rule_ts(int): Timestamp for rule data. If specified result contains rule data that has changed after this timestamp.
+        activator_ts(int): Timestamp for activator data.
+        cozify_uuid(str): Client application identifier.
+    Returns:
+        list: List of Delta objects indicating changes since the specified timestamps.
+    """
+    params = {}
+    if ts is not None:
+        params["ts"] = ts
+    if device_ts is not None:
+        params["deviceTs"] = device_ts
+    if group_ts is not None:
+        params["groupTs"] = group_ts
+    if scene_ts is not None:
+        params["sceneTs"] = scene_ts
+    if rule_ts is not None:
+        params["ruleTs"] = rule_ts
+    if activator_ts is not None:
+        params["activatorTs"] = activator_ts
+    if cozify_uuid is not None:
+        params["cozify_uuid"] = cozify_uuid
+
+    return get("/hub/poll", params=params, **kwargs)
